2029204d697a3a76503c5d3f76ca3fbdaa7051c1
imnixan <vcmvenom@gmail.com>
2023-12-14 23:48:13 +0700

    add button, gm, test mac

---
+++ Assets/Code/NewMonoBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] levels;
+    private void Awake()
+    {
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		Application.targetFrameRate = 300;
+
+    }
+
+    private void Start()
+    {
+        
+    }
+}
+
